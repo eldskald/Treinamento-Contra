a0e2805b5214fa7e23db0ad3eab1121a6a55d9ff
Ayrton <makkun1232@gmail.com>
2019-04-11 17:47:02 -0300

    Inimigos agora atiram
    Apenas trajetorias retas

--- Experiment/scripts/Enemy.gd
+++ Experiment/scripts/Enemy.gd
@@ -1,8 +1,12 @@
 extends KinematicBody2D
 
 export(float) var gravityAccel
+export(float) var shootCoolDown
 
+var internalTimer = 0
 var velocity = Vector2()
+var playerDirection = Vector2()
+var bullet = preload("res://prefabs/Enemy Projectile/Basic Enemy Projectile.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +16,21 @@ func _ready():
 func _process(delta):
 	velocity.y += gravityAccel*delta
 	velocity = move_and_slide(velocity)
+	if internalTimer >= shootCoolDown:
+		shoot()
+		internalTimer = 0
+	else:
+		internalTimer += delta
 #	pass
 
 func kill():
 	self.queue_free()
+
+func shoot():
+	playerDirection = get_parent().get_node_or_null("Player").position - position
+	playerDirection = playerDirection.normalized()
+	var projectile = bullet.instance()
+	projectile.direction = playerDirection
+	projectile.position = position + playerDirection*10
+	projectile.rotation += playerDirection.angle()
+	get_parent().add_child(projectile)
