e45161a5a61dee7f94b847d184af8ae1ba55a9bf
Rafael de Lima Bordoni <eldskald@callisto.eclipse.ime.usp.br>
2019-04-25 19:21:03 -0300

    Mais atualizacoes no menu

--- Experiment/scripts/Pause Menu.gd
+++ Experiment/scripts/Pause Menu.gd
@@ -1,10 +1,24 @@
 extends Control
 
+var submenuOpened = false
+var submenu
+
 func _physics_process(delta):
 	
 	# Unpausing.
+	if Input.is_action_just_pressed("ui_start") or Input.is_action_just_pressed("ui_select"):
+			get_tree().set_pause(false)
+			get_parent().gamePaused = false
+			self.queue_free()
 	
-	if Input.is_action_just_pressed("ui_start") or Input.is_action_just_pressed("ui_select") or Input.is_action_just_pressed("ui_cancel"):
+	# Closing opened submenus.
+	if Input.is_action_just_pressed("ui_cancel"):
+		if submenuOpened:
+			for menu in $Menus.get_children():
+				if menu.name != "Main Menu":
+					menu.queue_free()
+			submenuOpened = false
+		else:
 			get_tree().set_pause(false)
 			get_parent().gamePaused = false
 			self.queue_free()
@@ -16,3 +30,8 @@ func _on_resume_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_controls_pressed():
+	submenu = preload("res://prefabs/UI Elements/Controls Menu.tscn").instance()
+	$Menus.add_child(submenu)
+	submenuOpened = true
