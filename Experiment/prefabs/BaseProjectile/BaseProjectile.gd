211aa25341c11fdce0fb964b44c45c3857bc08bc
Ayrton <makkun1232@gmail.com>
2019-05-03 10:19:14 -0300

    Add BaseProjectile

---
+++ Experiment/prefabs/BaseProjectile/BaseProjectile.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+class_name BaseProjectile
+
+export(float) var speed
+export(float) var life_time
+export(String) var hurt_group
+
+export(int) var damage
+
+var direction:Vector2
+
+func _ready():
+	$LifeTimer.start(life_time)
+
+func _on_LifeTimer_timeout():
+	destroy()
+
+func destroy():
+	queue_free()
+
+
+func _on_HurtBox_body_entered(body):
+	if body.is_in_group(hurt_group):
+		body.take_damage(damage, direction)
+		destroy()
+		queue_free()
